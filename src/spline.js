211d2716fcfc8a480ff1b08cb14d5f682b00cef2
AlexanderPavlenko <AlexanderPavlenko@users.noreply.github.com>
2021-06-10 21:39:38 +0300

    some old changes

--- src/spline.js
+++ src/spline.js
@@ -5,6 +5,8 @@ require('aframe-extras');
 AFRAME.registerComponent('spline-scene', {
     init: function () {
         let scene = this.el.sceneEl;
+        // this allows to render Spline camera only which ignores headset movement and aframe entities
+        // scene.getElementsByTagName('canvas')[0].setAttribute('id', 'canvas3d');
         const SpeRuntime = require('./spline.runtime.min');
         const SPLINE = new SpeRuntime.Application();
         SPLINE.start('./scene.json');
